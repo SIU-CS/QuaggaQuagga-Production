b27020e46f0e0af445e55f5372f50a0e39344faa
N3rdizm <33732911+N3rdizm@users.noreply.github.com>
2018-03-19 17:42:01 -0500

    Adding textSearch

---
+++ js/display/textSearch.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+function textSearch() {
+    var input, filter, items, a, i;
+    input = document.getElementsByClassName("list-group-item");
+    filter = input.value.toUpperCase();
+    items = document.getElementsByClassName("list-group");
+
+    for (i = 0; i < items.length; i++) {
+        a = items[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            items[i].style.display = "";
+        } else {
+            items[i].style.display = "none";
+        }
+    }
+}
