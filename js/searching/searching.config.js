6428ebd0e1d4b3df326726af74fae7fd2e3385c2
N3rdizm <33732911+N3rdizm@users.noreply.github.com>
2018-04-23 17:50:12 -0500

    Finishing up fuzzy searching
    Added test file with test cases and cleaned up the fuzzySearch file itself

--- js/searching/searching.config.js
+++ js/searching/searching.config.js
@@ -16,16 +16,20 @@ function(require, getData, $, fuzzySearch, textSearch) {
     function handler(name) {
         var $ele = getData.getElementByName(name);
         var searchSettings = getData.getSettingByName("search", name);
-        var filterInterval = getData.getSettingByName("filterInterval", name);
 
-        if (searchSettings.filterInterval == null) {
-            filterInterval = 0;
+
+        // if there is no filterInterval defined in the search settings, default it to 0 for the purposes of the function
+        if (searchSettings.filterInterval == null || searchSettings.filterInterval == '') {
+            searchSettings.filterInterval = 0;
+        } else if (searchSettings.filterInterval > 1) {
+            searchSettings.filterInterval /= 100;
         }
+
 
         if ($ele == null) return;
 
         if (searchSettings.type == "fuzzy") {
-            fuzzySearch(name, $ele, searchSettings, filterInterval);
+            fuzzySearch(name, $ele, searchSettings, searchSettings.filterInterval);
         } else {
             textSearch(name, $ele, searchSettings);
         }
