4bd8da0770a882e820ebab899375258101b00bc8
james-wasson <jameswasson1@gmail.com>
2018-05-01 09:55:17 -0500

    a

--- dist/main.js
+++ dist/main.js
@@ -2672,72 +2672,9 @@ define('utility/showHideItems',['require',
 
 define('searching/searchHelper',['require', 'jquery', 'data_store/get', 'utility/showHideItems'],
     function (require, $, getData, showHideItems) {
-<<<<<<< HEAD
-=======
     'use strict';
 
     var jquery = $;
-
-    var searchByFunction = function(determineSearch, data, isCaseSensitive, filterInterval) {
-        var returnVisible = false;
-        for (var i = 0; i < data.length; i += 1) {
-            var item = data[i];
-
-            var name = item['@name'];
-            var searchable = item['@searchable'];
-            if (!isCaseSensitive) {
-                name = name.toLowerCase();
-                searchable = searchable.toLowerCase();
-            }
-            if (filterInterval == null || filterInterval == '') {
-                filterInterval = 0;
-            }
-
-            // if input is not empty, and the input matches an entry in the 
-            // multiselect, show the item and its children
-            if (determineSearch(name, searchable)) {
-                showHideItems.showItem(item);
-                showHideItems.showAllChildren(item);
-                returnVisible = true;
-            } else {
-                if (item['@isHeader']) {
-                    // recursively performs the plain text search on the child items 
-                    // to check if they are visible
-                    var isAnyVisible = searchByFunction(
-                        determineSearch, 
-                        item['@children'], 
-                        isCaseSensitive,
-                        filterInterval
-                    );
-                    if (isAnyVisible) {
-                        showHideItems.showItem(item);
-                        returnVisible = true;
-                    } else {
-                        showHideItems.hideItem(item);
-                    }
-                } else {
-                    showHideItems.hideItem(item);
-                }
-            }
-        }
-        return returnVisible;
-    };
-    return {
-        searchByFunction: searchByFunction,
-        clearSearch: function($multi) {
-            var $searchBar = $multi.find(".JSM-head .JSM-search .JSM-searchbar");
-            $searchBar.val("");
-            $searchBar.trigger("keyup");
-        }
-    };
-});
-define('style/popover',['require', 'jquery', 'data_store/get', 'data_store/set', 'style/body/spaceIndent', 'searching/searchHelper'], 
-function(require, $, getData, setData, spaceIndent, searchHelper) {
->>>>>>> dev
-    'use strict';
-
-    var jquery = $;
-<<<<<<< HEAD
 
     var searchByFunction = function(determineSearch, data, isCaseSensitive, filterInterval) {
         var returnVisible = false;
@@ -2801,32 +2738,6 @@ function(require, $, getData, setData, spaceIndent, searchHelper) {
 
     // Function to Display popovers after selection
     function Popup(item, $multiselect){
-=======
-    var itemNum = 0;
-
-    function showHideHandler($multiselect, $popDisplay, onClose) {
-        var $searchBar = $multiselect.find(".JSM-head .JSM-search .JSM-searchbar");
-        $searchBar.on("focus", function() {
-            $popDisplay.empty();
-            $popDisplay.collapse("hide");
-        // Display list when search bar is on focus 
-            $multiselect.find(".JSM-list.collapse").collapse("show");
-            spaceIndent.refresh($multiselect);
-        });
-        var $close = $multiselect.find(".JSM-head .JSM-closePopList");
-        $close.on("click", function() {
-        // Hide list when ClosePoplist is clicked and show the selected items as popovers
-            $multiselect.find(".JSM-list.collapse").collapse("hide");
-            $popDisplay.empty();
-            $popDisplay.collapse("show");
-            searchHelper.clearSearch($multiselect);
-            if (onClose != null) onClose();
-        });
-    }
-        // Function to Display popovers after selection
-    function Popup(item, $popDisplay){
-        $popDisplay.append(
->>>>>>> dev
         // Popover Basic style
         var poppedItem = $(            
             '<span class="JSM-popover">'+
